4a7a53c5ad642bdc741e9ec0817386cbf22f546f
Nathan Perfetti <nathanperfetti@gmail.com>
2021-09-27 00:18:20 -0500

    exported spotify api client to module

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const cors = require('cors');
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 7001;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
 
-const {} = require('./utils/spotifyAPI.mjs')
-app.get((req,res) => {
+app.get('/',(req,res) => {
 
   res.json()
 })
--- utils/spotifyAPI.js
+++ utils/spotifyAPI.js
@@ -4,108 +4,66 @@ const btoa = require("btoa");
 const clientId = process.env.SPOTIFY_CLIENT_ID;
 const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
+getSpotifyToken = async () => {
+  const headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+  };
+  const body = "grant_type=client_credentials";
 
-  _getToken = async () => {
-    const headers = {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
-    };
-    const body = "grant_type=client_credentials";
-
-    axios
-      .post("https://accounts.spotify.com/api/token", body, {
+  try {
+    const tokenResponse = await axios.post(
+      "https://accounts.spotify.com/api/token",
+      body,
+      {
         headers: headers,
-      })
-      .then((tokenResponse) => {
-        console.log(tokenResponse.data.access_token);
-        return tokenResponse.data.access_token;
-      })
-      .catch((error) => {
-        console.log(error);
-        return null;
-      });
-  }
+      }
+    );
 
-  _getGenres = async (token) => {
-    const result = await fetch(
-      `https://api.spotify.com/v1/browse/categories?locale=sv_US`,
+    return tokenResponse.data.access_token;
+
+  } catch (error) {
+    console.log(error);
+    return null;
+
+  }
+}; // end getSpotifyToken fct def
+
+getSpotifyTracks = async (getToken, isrcCode) => {
+  const token = await getToken();
+  const limit = 10;
+
+  try {
+    const tracksResponse = await axios.get(
+      `https://api.spotify.com/v1/search?type=track&q=isrc:${isrcCode}&limit=${limit}`,
       {
-        method: "GET",
         headers: { Authorization: "Bearer " + token },
       }
     );
 
-    const data = await result.json();
-    console.log(data);
-    //return data.categories.items;
+    //Search result items are already ordered by popularity, use first result in array
+    let tracksData = {
+      spotifyId: tracksResponse.data.tracks.items[0].id,
+      title: tracksResponse.data.tracks.items[0].name,
+      artists: tracksResponse.data.tracks.items[0].artists,
+      albumUrl: tracksResponse.data.tracks.items[0].album.images[0].url,
+    };
+    console.log("\n \n======================== \n")
+    console.log("+++> Insert into DB: \n", tracksData);
+
+    return tracksData.items;
+
+  } catch (error) {
+    console.log(error);
+    return null;
   }
+}; // end getSpotifyTracks fct def
 
-  _getPlaylistByGenre = async (token, genreId) => {
-    const limit = 10;
+getSpotifyTracks(getSpotifyToken, "GBAYE6700149");
+getSpotifyTracks(getSpotifyToken, "USVT10300001");
+getSpotifyTracks(getSpotifyToken, "USEE10001992");
+getSpotifyTracks(getSpotifyToken, "GBAYE0601498");
+getSpotifyTracks(getSpotifyToken, "USWB11403680");
+getSpotifyTracks(getSpotifyToken, "GBAYE0601477");
 
-    const result = await fetch(
-      `https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`,
-      {
-        method: "GET",
-        headers: { Authorization: "Bearer " + token },
-      }
-    );
-
-    const data = await result.json();
-    return data.playlists.items;
-  }
-
-  //https://api.spotify.com/v1/search?type=track&q=isrc:USWB11802164+OR+isrc:DEMA61301103
-  _getTracks = async (token, tracksEndPoint) => {
-    const limit = 10;
-
-    axios
-      .get(`${tracksEndPoint}&limit=${limit}`, {
-        headers: { Authorization: "Bearer " + token },
-      })
-      .then((tracksResponse) => {
-
-        console.log(tracksResponse.data.track);
-
-        let tracksData = {
-          spotifyId: tracksResponse.data.tracks.items[0].id,
-          title: tracksResponse.data.tracks.items[0].name,
-          artists: tracksResponse.data.tracks.items[0].artists,
-          albumUrl: tracksResponse.data.tracks.items[0].album.images[0].url,
-        };
-
-        console.log("+++> Insert into DB: \n",tracksData);
-
-        return tracksData.items;
-      })
-      .catch((error) => {
-        console.log(error);
-        return null;
-      });
-
-    //Spotify Image URI
-    // Title
-    // Artist Name List
-    //In case the SpotifyAPI returns multiple tracks take the track with highest popularity (an attribute in the Json)
-
-  }
-
-  _getTrack = async (token, trackEndPoint) => {
-    const result = await fetch(`${trackEndPoint}`, {
-      method: "GET",
-      headers: { Authorization: "Bearer " + token },
-    });
-
-    const data = await result.json();
-    return data;
-  }
-
-
-// SpotifyAPI._getTracks(
-// await SpotifyAPI._getToken(),
-//   "https://api.spotify.com/v1/search?type=track&q=isrc:GBAYE6700149"
-// );
-
- _getTracks('BQCVp95rp5Njc8rHVdt5jVD6AUgFpwpQ2Ktdd5iUG1x97D5m0ZH82VTjttqz7dmM4EVD2hxbjs94ctiFh1E',"https://api.spotify.com/v1/search?type=track&q=isrc:GBAYE6700149");
-
-//module.exports = SpotifyAPI;
+module.exports = { getSpotifyToken, getSpotifyTracks };
